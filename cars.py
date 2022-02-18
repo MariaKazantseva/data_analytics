e4ed6c0577ec9cb62d2cab20592a92081e0527fd
Masha <69643036+MariaKazantseva@users.noreply.github.com>
2022-02-18 13:15:39 +0100

    Update cars.py

--- cars.py
+++ cars.py
@@ -3,13 +3,6 @@ import numpy as np
 from datetime import datetime, timedelta, date
 
 
-'''1. Clients who have Mitsubishi or Renault
-2. Clients who have more than 2 cars
-3. Cars no older than 5 years
-4. Clients who visited more than twice 
-5. Clients who visited our store no longer than 3 months ago'''
-
-
 def old(cell):
     if cell == 'unknown':
         return -1
